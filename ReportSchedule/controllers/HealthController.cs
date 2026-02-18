277234d07a46f27b45e63bfef6b6132332aa1bcc
Cassio Da Silva De Jesus <cassios@g.globo>
2026-02-17 23:03:48 -0300

    Enviando projeto inicial com rota health

---
+++ ReportSchedule/controllers/HealthController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportSchedule.controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "ok" });
+    }
+}
